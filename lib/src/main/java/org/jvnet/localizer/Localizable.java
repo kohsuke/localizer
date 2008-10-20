ba0a8c406f44532d09bb309c25bd824ba004b2ba
stephenconnolly <null>
2008-10-20 17:59:36 +0000

    Make Localizable Serializable, need to be able to handle when the arguments are non-serializable as these objects may be created on one tier and handed over to another via a form of RMI

--- lib/src/main/java/org/jvnet/localizer/Localizable.java
+++ lib/src/main/java/org/jvnet/localizer/Localizable.java
@@ -2,6 +2,7 @@ package org.jvnet.localizer;
 
 import java.util.Locale;
 import java.text.MessageFormat;
+import java.io.Serializable;
 
 /**
  * Captures the localizable string. Can be converted to a string just by
@@ -9,15 +10,26 @@ import java.text.MessageFormat;
  *
  * @author Kohsuke Kawaguchi
  */
-public class Localizable {
+public class Localizable implements Serializable {
     private final ResourceBundleHolder holder;
     private final String key;
-    private final Object[] args;
+    private final Serializable[] args;
 
     public Localizable(ResourceBundleHolder holder, String key, Object... args) {
         this.holder = holder;
         this.key = key;
-        this.args = args;
+        this.args = new Serializable[args.length];
+        for (int i = 0; i < args.length; i++) {
+            if (args[i] instanceof Serializable) {
+                this.args[i] = (Serializable)args[i];
+            } else {
+                // MessageFormat only supports formats of "number", "date", "time" and "choice"
+                // All of which will be formatting objects that must be Serializable
+                // Anything else will just have it's toString() method invoked
+                // by MessageFormat, so we'll just call toString up front.
+                this.args[i] = args[i].toString();
+            }
+        }
     }
 
     public String toString(Locale locale) {
