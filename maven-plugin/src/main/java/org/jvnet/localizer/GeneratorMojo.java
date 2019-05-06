3000ed56c3851a5b6f24ecd869220f7d7d430590
Tőzsér Tamás <tamas.tozser@doctusoft.com>
2019-05-06 15:04:11 +0200

    update property syntax
    org.jvnet.localizer.GeneratorMojo#project:
      The syntax
        @parameter expression="${property}"
      is deprecated, please use
        @parameter property="property"
      instead.

--- maven-plugin/src/main/java/org/jvnet/localizer/GeneratorMojo.java
+++ maven-plugin/src/main/java/org/jvnet/localizer/GeneratorMojo.java
@@ -48,7 +48,7 @@ public class GeneratorMojo extends AbstractMojo {
     /**
      * The maven project.
      *
-     * @parameter expression="${project}"
+     * @parameter property="project"
      * @required
      * @readonly
      */
